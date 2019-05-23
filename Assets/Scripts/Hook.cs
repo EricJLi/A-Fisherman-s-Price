cdc2578c90db5bb3181b31a553802ad2b34bc341
EricJLi <li.eric1998@gmail.com>
2019-05-23 15:44:27 -0400

    Add Sprites and Scrolling Background

---
+++ Assets/Scripts/Hook.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour {
+
+    [SerializeField] private float lineSpeed;
+    private LineRenderer lineRenderer;
+    private Vector2 startPos;
+    private bool isDragging;
+    private Vector2 touchDelta;
+
+    private Vector3 min;
+    private Vector3 max;
+
+    void Start() {
+        lineRenderer = GetComponent<LineRenderer>();
+        min = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        max = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+    }
+
+    void Update() {
+        // if (Input.touchCount > 0) {
+        //     Touch touch = Input.GetTouch(0);
+        //     if (touch.phase == TouchPhase.Began) {
+        //         startPos = touch.position;
+        //     } else if (touch.phase == TouchPhase.Moved) {
+        //         touchDelta = touch.position - startPos;
+        //     } else if (touch.phase == TouchPhase.Ended) {
+        //         startPos = new Vector2(0, 0);
+        //         isDragging = false;
+        //         touchDelta = new Vector2(0, 0);
+        //     }
+        // }
+
+        if (Input.GetMouseButtonDown(0) && !isDragging) {
+            startPos = Input.mousePosition;
+            isDragging = true;
+        }
+
+        if (isDragging) {
+            touchDelta = (Vector2)Input.mousePosition - startPos;
+            startPos = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            startPos = new Vector2(0, 0);
+            isDragging = false;
+            touchDelta = new Vector2(0, 0);
+        }
+
+        Move();
+        lineRenderer.SetPosition(0, Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 1)));
+        lineRenderer.SetPosition(1, transform.position);
+    }
+
+    private void Move() {
+        var deltaX = touchDelta.x * Time.deltaTime * lineSpeed;
+        var deltaY = touchDelta.y * Time.deltaTime * lineSpeed;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, min.x, max.x);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, min.y, max.y);
+        transform.position = new Vector3(newXPos, newYPos);
+    }
+}
---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+    [SerializeField] private float bgSpeed;
+    [SerializeField] Sprite[] bgSprites;
+    [SerializeField] int[] reps;
+    [SerializeField] GameObject bgPrefab;
+    List<Background> backgrounds;
+    bool isEmpty = false;
+    bool done = false;
+
+    public class Background {
+        public Background(Sprite s, int r) {
+            sprite = s;
+            reps = r;
+        }
+        public Sprite sprite;
+        public int reps;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        backgrounds = new List<Background>();
+        for (int x = 0; x < bgSprites.Length; x++) {
+            backgrounds.Add(new Background(bgSprites[x], reps[x]));
+        }
+        bgPrefab.GetComponent<SpriteRenderer>().sprite = getNextBackground();
+        Instantiate(bgPrefab, transform);
+        addBackground(transform);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!done) {
+            Move();
+        }
+    }
+
+    void Move() {
+        foreach (Transform child in transform) {
+            if (isEmpty && child.position.y <= transform.position.y &&
+                child.position.y + bgSpeed > transform.position.y && transform.childCount == 2) {
+                done = true;
+                break;
+            }
+            child.position += new Vector3(0, bgSpeed, 0);
+            if (child.position.y >= Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y &&
+                (child.position.y - bgSpeed) < Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y) {
+                addBackground(child);
+            }
+            if (child.position.y > Camera.main.ViewportToWorldPoint(new Vector3(0, 2, 0)).y) {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+
+    void addBackground(Transform t) {
+        Sprite s = getNextBackground();
+        if (s == null) {
+            isEmpty = true;
+            return;
+        }
+        Vector3 newPos = t.position;
+        newPos.y -= Camera.main.ViewportToWorldPoint(new Vector3(0, 1.5f, 0)).y;
+        bgPrefab.GetComponent<SpriteRenderer>().sprite = s;
+        Instantiate(bgPrefab, newPos, transform.rotation, transform);
+    }
+
+    Sprite getNextBackground() {
+        for (int x = 0; x < backgrounds.Count; x++) {
+            if (backgrounds[x].reps > 0) {
+                backgrounds[x].reps --;
+                return backgrounds[x].sprite;
+            }
+        }
+        return null;
+    }
+}
